a31fb970ed3d772c4fe6bb12e4dfe8727059004c
Waorange <771109835@qq.com>
2018-06-03 14:54:33 +0800

    Waorange

--- Sort/Sort.c
+++ Sort/Sort.c
@@ -3,6 +3,9 @@
 #include <stdio.h>
 #include <string.h>
 #include "stack.h"
+#include <time.h>
+
+#define N 1000000
 
 
 static void _Swap(int * a, int * b)
@@ -14,273 +17,336 @@ static void _Swap(int * a, int * b)
 
 /////////////////////////////////////////////////////////
 //从小到大
-void InsretSort(int * a, size_t n)
+void InsretSort(int * array, size_t n)
 {
-	assert(a);
-	for (size_t i = 0; i < n - 1; ++i)
+	assert(array);
+	for (size_t i = 1; i < n; ++i)  //[1，n)
 	{
-		int end = i;
-		int tmp = a[end + 1];
-		while (end >= 0)
+		int end = i - 1;   //[0，end]为有序区间
+		int cur = array[i];
+		while (end >= 0 && array[end] > cur)
 		{
-			if (a[end] > tmp)
-			{
-				a[end + 1] = a[end];
-				end--;
-			}
-			else
-			{
-				a[end + 1] = tmp;
-				break;
-			}
+			array[end + 1] = array[end--];//元素后移
 		}
-		if (end == -1)
-		{
-			a[end + 1] = tmp;
-		}
+		array[end + 1] = cur;
 	}
 }
 
-
 //////////////////////////////////////////////////////
 //从小到大
-void ShallSort(int * a, size_t n)
+//void ShallSort(int * array, size_t n)
+//{
+//	assert(array);
+//	for(int gap = n / 2 + 1; gap > 0; gap /= 2)
+//	{
+//		for (size_t i = 0; i < n - gap; i++)
+//		{
+//			int end = i;
+//			int tmp = array[end + gap];
+//			while (end >= 0)
+//			{
+//				if (array[end] > tmp)
+//				{
+//					array[end + gap] = array[end];
+//					end -= gap;
+//				}
+//				else
+//				{
+//					array[end + gap] = tmp;
+//					break;
+//				}
+//			}
+//			if (end < 0)
+//			{
+//				array[end + gap] = tmp;
+//			}
+//		}
+//	}
+//}
+
+void ShallSort(int * array, size_t n)
 {
-	assert(a);
-	for(int gap = n / 2 + 1; gap > 0; gap /= 2)
+	assert(array);
+	for (int gap = n >> 1; gap > 0; gap >>= 1) //步长选择为n/2并对步长取半达到1
 	{
-		for (size_t i = 0; i < n - gap; i++)
+		for (size_t i = gap; i < n; i++)
 		{
-			int end = i;
-			int tmp = a[end + gap];
-			while (end >= 0)
+			int end = i - gap;
+			int cur = array[i];
+			while (end >= 0 && array[end] > cur)
 			{
-				if (a[end] > tmp)
-				{
-					a[end + gap] = a[end];
-					end -= gap;
-				}
-				else
-				{
-					a[end + gap] = tmp;
-					break;
-				}
+				array[end + gap] = array[end];
+				end -= gap;
 			}
-			if (end < 0)
-			{
-				a[end + gap] = tmp;
-			}
+			array[end + gap] = cur;
 		}
-	}
+	}//gap
 }
 
 
 /////////////////////////////////////////////////////////////////////////////
-void SelectSort(int * a, size_t n)
+void SelectSort(int * array, size_t n)
 {
-	assert(a);
+	assert(array);
 	int min = 0, max = 0;
+
 	for (size_t left = 0, right = n - 1; left < right; left++, right--)
 	{
 		min = left;
 		max = right;
-		for (size_t i = left; i <= right; ++i)
+		for (size_t i = left; i <= right; ++i) //遍历未排序的元素  
 		{
-			if (a[i] < a[min])
+			if (array[i] < array[min])  //找到最小的元素下标
 			{
 				min = i;
 			}
-			if (a[i] > a[max])
+			if (array[i] > array[max])  //找最大的元素下标
 			{
 				max = i;
 			}
 		}
-		if (min != left)
+		if (min != left)  //最小的元素放在左边
 		{
-			_Swap(&a[min], &a[left]);
+			_Swap(&array[min], &array[left]);
 		}
-		if (max != right && !(min == right && max == left))
+		if (max == left) //如果最大的元素在最小元素位置，将max改为最小元素交换后的位置
 		{
-			_Swap(&a[max], &a[right]);
+			max = min;
+		}
+		if (max != right) //将最大的元素放在右边
+		{
+			_Swap(&array[max], &array[right]);
 		}
 	}
 }
 
 ///////////////////////////////////////////////////////////////
-static void AdjustDown(int * a, size_t n, int root)
+static void AdjustDown(int * array, size_t n, int root) //向下调整
 {
-	assert(a);
+	assert(array);
+	//在阵列起始位置为0的情形中：
+
+	//父节点i的左子节点在位置(2i+1);
+	//父节点i的右子节点在位置(2i+2);
+	//子节点i的父节点在位置 (i-1)/2);
 	int parents = root;
 	int child = parents * 2 + 1;
+
 	while (child < (int)n)
 	{
-		if (child + 1 < (int)n && a[child] < a[child + 1])
+		if (child + 1 < (int)n && array[child] < array[child + 1])  //找到左右孩子大的一个
 		{
 			child++;
 		}
-		if (a[child] > a[parents])
+		if (array[child] > array[parents]) //和父亲节点进行比较如果父亲节点小进行交换继续调整其字树
 		{
-			_Swap(&a[parents], &a[child]);
+			_Swap(&array[parents], &array[child]);
 			parents = child;
 			child = parents * 2 + 1;
 		}
-		else
+		else //如果其满足大堆直接跳出
 		{
 			break;
 		}
 	}
 }
 
-void HeapSort(int * a, size_t n)
+void HeapSort(int * array, size_t n)
 {
-	assert(a);
+	assert(array);
+
+	//建堆
 	for (int i = ((n - 2) >> 1); i >= 0; --i)
 	{
-		AdjustDown(a, n, i);
+		AdjustDown(array, n, i);
 	}
+
+	//进行排序
 	int end = n - 1;
 	while (end > 0)
 	{
-		_Swap(&a[end], &a[0]);
-		AdjustDown(a, end, 0);
+		_Swap(&array[end], &array[0]); //和最后一个进行交换
+		AdjustDown(array, end, 0); //重新调整成大堆
+		end--;
+	}
+}
+/////////////////////////////////////////////////////////////////////
+
+void BubbleSort(int * a, int n)
+{
+	assert(a);
+	int end = n - 1;
+	while (end > 0)        //控制冒泡结束的位置
+	{
+		int flag = 0;  //设标志
+		for (int i = 0; i < end; ++i) //在区间 [0, end)
+		{
+			if (a[i] > a[i + 1])
+			{
+				_Swap(&a[i], &a[i+1]);
+				flag = 1;  //如果交换标志就设为1
+			}
+		}
+		if (flag == 0) //如果没有交换就说明有序直接结束
+		{
+			break;
+		}
 		end--;
 	}
 }
 
 /////////////////////////////////////////////////////////////////////
-static int GetMin(int * a, int left, int right)
+
+int GetMin(int x[], int a, int b, int c)
 {
-	assert(a);
-	int mid = left + ((right - left) >> 1);
-	if (a[mid] < a[left])
-	{
-		if (a[mid] < a[right]) //a[mid]最小
-		{
-			if (a[right] > a[left])
-			{
-				return left;
-			}
-			else
-			{
-				return right;
-			}
-		}
-		else //a[mid]为中间值
-		{
-			return mid;
-		}
-	} 
-	else  //a[mid] > a[left]
-	{
-		if (a[mid] < a[right]) //a[mid]为中间值
-		{
-			return mid;
-		}
-		else //a[mid]最大
-		{
-			if (a[left] > a[right])
-			{
-				return right;
-			}
-			else
-			{
-				return left;
-			}
-		}
-	}
+	return x[a] < x[b] ? (x[b] < x[c] ? b : x[a] < x[c] ? c : a)
+		: x[b] > x[c] ? b : x[a] > x[c] ? c : a;
 }
 
+////三数取中法
+//static int GetMin(int * array, int left, int right)
+//{
+//	assert(array);
+//	int mid = left + ((right - left) >> 1);
+//	if (array[mid] < array[left])
+//	{
+//		if (array[mid] < array[right]) //a[mid]最小
+//		{
+//			if (array[right] > array[left])
+//			{
+//				return left;
+//			}
+//			else
+//			{
+//				return right;
+//			}
+//		}
+//		else //a[mid]为中间值
+//		{
+//			return mid;
+//		}
+//	} 
+//	else  //a[mid] > a[left]
+//	{
+//		if (array[mid] < array[right]) //a[mid]为中间值
+//		{
+//			return mid;
+//		}
+//		else //a[mid]最大
+//		{
+//			if (array[left] > array[right])
+//			{
+//				return right;
+//			}
+//			else
+//			{
+//				return left;
+//			}
+//		}
+//	}
+//}
+
 //左右指针法
-int PartSort1(int * a, int left, int right)
+int PartSort1(int * array, int left, int right)
 {
-	assert(a);
+	assert(array);
 	//使用三数取中法规避数据一边倒
-	int min = GetMin(a, left, right);
-	_Swap(&a[min], &a[right]);   
+	int mid = GetMin(array, left, left + ((right - left) >> 1), right);
+	_Swap(&array[mid], &array[right]);   
 	
-	int cmp = right;//选数组最右边值为枢纽值
+	int pivot = right;//选数组最右边值为枢纽值
 
 	while (left < right) //当左右相遇时结束交换枢纽值
 	{
 		//左边遇到比枢纽值小的和相等值往后走， 遇到大于枢纽值的的值停止
-		while (left < right && a[left] <= a[cmp]) 
+		while (left < right && array[left] <= array[pivot]) 
 		{
 			left++;
 		}
 		//右边遇到比枢纽值大的和相等值往后走， 遇到小于枢纽值的的值停止	
-		while (left < right && a[right] >= a[cmp])
+		while (left < right && array[right] >= array[pivot])
 		{
 			right--;
 		}
 		//将左右值交换 使小于枢纽值的放在左边， 大于枢纽值的放在右边
-		_Swap(&a[left], &a[right]);
+		_Swap(&array[left], &array[right]);
 	}
 	//找到枢纽值的位置并和枢纽值交换
-	_Swap(&a[left], &a[cmp]);
+	_Swap(&array[left], &array[pivot]);
 	return left;
 }
 
 //挖坑法
-int PartSort2(int * a, int left, int right)
+int PartSort2(int * array, int left, int right)
 {
-	assert(a);
-	assert(a);
+	assert(array);
 	//选数组最右边值为枢纽值,使用三数取中法规避数据一边倒
-	int min = GetMin(a, left, right);
-	_Swap(&a[min], &a[right]);
+	int mid = GetMin(array, left, left + ((right - left) >> 1), right);
+	_Swap(&array[mid], &array[right]);
 
 	//第一次最右面为坑
-	int cmp = a[right];
+	int pivot = array[right];
 	while (left < right)
 	{
-		while (left < right && a[left] <= cmp)
+		while (left < right && array[left] <= pivot)
 		{
 			left++;
 		}
-		a[right] = a[left]; //将右边坑填上后左边为坑
-		while (left < right && a[right] >= cmp)
+		array[right] = array[left]; //将右边坑填上后左边为坑
+		while (left < right && array[right] >= pivot)
 		{
 			right--;
 		}
-		a[left] = a[right]; //将左边坑填上之后右边为坑
+		array[left] = array[right]; //将左边坑填上之后右边为坑
 	}
-	a[left] = cmp;//填上左右相遇点的坑
+	array[left] = pivot;//填上左右相遇点的坑
 	return left;
 }
 
 //前后指针法
-int PartSort3(int * a, int left, int right)
+int PartSort3(int * array, int left, int right)
 {
-	assert(a);
+	assert(array);
+	//选数组最右边值为枢纽值,使用三数取中法规避数据一边倒
+	int mid = GetMin(array, left, left + ((right - left) >> 1), right);
+	_Swap(&array[mid], &array[right]);
+
 	int prev = left - 1, cur = left;
 	while (cur < right)
 	{
-		if (a[cur] <= a[right] && ++prev != cur )
+		if (array[cur] <= array[right] && ++prev != cur )
 		{
-			_Swap(&a[prev], &a[cur]);
+			_Swap(&array[prev], &array[cur]);
 		}
 		cur++;
 	}
-	_Swap(&a[right], &a[++prev]);
+	_Swap(&array[right], &array[++prev]);
 	return prev;
 }
 
 
-void QuickSort(int * a, int left, int right)
+void QuickSort(int * array, int left, int right)
 {
-	assert(a);
+	assert(array);
 	if (left >= right)
 	{
 		return;
 	}
-	int div = PartSort1(a, left, right);
-	QuickSort(a, left, div - 1);
-	QuickSort(a, div + 1, right);
+
+	if (right - left + 1 == 7)
+	{
+		InsretSort(array, right - left + 1);
+		return;
+	}
+	int div = PartSort3(array, left, right);
+
+	QuickSort(array, left, div - 1);
+	QuickSort(array, div + 1, right);
 }
 
-void QuickSortNR(int * a, int left, int right)
+void QuickSortNR(int * array, int left, int right)
 {
-	assert(a);
+	assert(array);
 	Stack S;
 	StackInit(&S, 10);
 
@@ -295,8 +361,13 @@ void QuickSortNR(int * a, int left, int right)
 		StackPop(&S);
 		int _left = StackTop(&S);
 		StackPop(&S);
-
-		int div = PartSort1(a, _left, _right);
+		//如果小于阀值7就使用插入排序
+		if (right - left + 1 == 7)
+		{
+			InsretSort(array, right - left + 1);
+			continue;
+		}
+		int div = PartSort1(array, _left, _right);
 
 		if (_left < div - 1)
 		{
@@ -311,9 +382,11 @@ void QuickSortNR(int * a, int left, int right)
 	}
 }
 
-void _MergeSort(int * a, int left, int right, int * tmp)
+////////////////////////////////////////////////////////////////////
+
+void _MergeSort(int * array, int left, int right, int * tmp)
 {
-	assert(a);
+	assert(array);
 	if (left >= right)
 	{
 		return;
@@ -321,8 +394,8 @@ void _MergeSort(int * a, int left, int right, int * tmp)
 	int mid = left + ((right - left) >> 1);
 
 	//分为[left mid] [mid + 1 right]两个区间
-	_MergeSort(a, left, mid, tmp);
-	_MergeSort(a, mid + 1, right, tmp);
+	_MergeSort(array, left, mid, tmp);
+	_MergeSort(array, mid + 1, right, tmp);
 
 	//至此左右区间均有序
 	int left_a = left, right_a = mid;
@@ -331,40 +404,41 @@ void _MergeSort(int * a, int left, int right, int * tmp)
 	while (left_a <= right_a && left_b <= right_b)
 	{
 		//从小到大排序
-		if (a[left_a] > a[left_b])
+		if (array[left_a] > array[left_b])
 		{
-			tmp[index++] = a[left_b++];
+			tmp[index++] = array[left_b++];
 		}
 		else
 		{
-			tmp[index++] = a[left_a++];
+			tmp[index++] = array[left_a++];
 		}
 	}
 	while (left_a <= right_a)
 	{
-		tmp[index++] = a[left_a++];
+		tmp[index++] = array[left_a++];
 	}
 	while (left_b <= right_b)
 	{
-		tmp[index++] = a[left_b++];
+		tmp[index++] = array[left_b++];
 	}
-	memcpy(&a[left], &tmp[left], sizeof(int)* (right - left + 1));
+	memcpy(&array[left], &tmp[left], sizeof(int)* (right - left + 1));
 }
 
-void MergeSort(int * a, int left, int right)
+void MergeSort(int * array, int left, int right)
 {
-	assert(a);
+	assert(array);
 	int * tmp = (int *)malloc(sizeof(int) * (right - left + 1));
-	_MergeSort(a, left, right, tmp);
+	_MergeSort(array, left, right, tmp);
 	free(tmp);
 }
 
-void MergeSortNR(int * a, int left, int right)
+
+void MergeSortNR(int * array, int left, int right)
 {
-	assert(a);
+	assert(array);
 	int * tmp = (int *)malloc(sizeof(int) * (right - left + 1));
 
-	for(int gap = 1; gap <= (right - left + 1); gap <<= 1)
+	for (int gap = 1; gap <= (right - left + 1); gap <<= 1)
 	{
 		int cur = left;
 		while (cur <= right)
@@ -372,7 +446,6 @@ void MergeSortNR(int * a, int left, int right)
 			//计算区间中点分为两个区间 [cur mid - 1] [mid mid + gap - 1]
 			int mid = cur + gap;
 
-			
 			int left_a = cur, right_a = mid - 1;
 			if (right_a >= right)
 			{
@@ -389,26 +462,28 @@ void MergeSortNR(int * a, int left, int right)
 			while (left_a <= right_a && left_b <= right_b)
 			{
 				//从小到大排序
-				if (a[left_a] > a[left_b])
+				if (array[left_a] > array[left_b])
 				{
-					tmp[index++] = a[left_b++];
+					tmp[index++] = array[left_b++];
 				}
 				else
 				{
-					tmp[index++] = a[left_a++];
+					tmp[index++] = array[left_a++];
 				}
 			}
 
 			while (left_a <= right_a)
 			{
-				tmp[index++] = a[left_a++];
+				tmp[index++] = array[left_a++];
 			}
 			while (left_b <= right_b)
 			{
-				tmp[index++] = a[left_b++];
+				tmp[index++] = array[left_b++];
 			}
-			int count_bit = (gap << 1) > (right - left + 1) ? (right - left + 1) : (gap << 1);
-			memcpy(&a[cur], &tmp[cur], sizeof(int)* count_bit);
+			////计算需要拷贝的空间字节大小
+			int count_byte = (right_b - cur + 1) < (gap << 1) ? (right_b - cur + 1) : (gap << 1);
+			memcpy(&array[cur], &tmp[cur], sizeof(int)* count_byte);
+
 			cur += (gap << 1);
 		}//while(cur <= right)
 	}
@@ -417,68 +492,33 @@ void MergeSortNR(int * a, int left, int right)
 
 
 ////////////////////////////////////////////////////////////////////
-void TestMergeSort()
+
+
+
+void TestSort()
 {
 	//int a[] = { 2, 5, 8, 4, 3, 1, 9, 6 };
-	int a[] = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
-	MergeSortNR(a, 0, sizeof(a) / sizeof(int) - 1);
-	//MergeSort(a, 0, sizeof(a) / sizeof(int)- 1);
-	for (size_t i = 0; i < sizeof(a) / sizeof(int); ++i)
-	{
-		printf("%d ", a[i]);
-	}
-}
-
-void TestHeapSort()
-{
-	int a[] = { 2, 5, 8, 4, 3, 1, 9, 6 };
 	//int a[] = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
-	HeapSort(a, sizeof(a) / sizeof(int));
-	for (size_t i = 0; i < sizeof(a) / sizeof(int); ++i)
+	int * a;
+	a = (int *)malloc(sizeof(int) * N);
+	for (int i = 0; i < N; ++i)
 	{
-		printf("%d ", a[i]);
+		a[i] = rand();
 	}
-}
+	//InsretSort(a, N);
+	//ShallSort(a, N);
 
-void TestQuickSort()
-{
-	//int a[] = { 2, 5, 8, 4, 3, 1, 9, 6 };
-	int a[] = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
-	//QuickSort(a, 0,  sizeof(a) / sizeof(int) - 1);
-	QuickSortNR(a, 0,  sizeof(a) / sizeof(int) - 1);
+	//SelectSort(a, N);
+	//HeapSort(a, N);
 
-	for (size_t i = 0; i < sizeof(a) / sizeof(int); ++i)
-	{
-		printf("%d ", a[i]);
-	}
-}
+	//BubbleSort(a, N);
+	//QuickSort(a, 0, N - 1);
+	//QuickSortNR(a, 0, N - 1);
 
-void TestSelectSort()
-{
-	//int a[] = { 2, 5, 8, 4, 3, 1, 9, 6 };
-	int a[] = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
-	SelectSort(a, sizeof(a) / sizeof(int));
-	for (size_t i = 0; i < sizeof(a) / sizeof(int); ++i)
-	{
-		printf("%d ", a[i]);
-	}
-}
+	//MergeSort(a, 0, N - 1);
+	MergeSortNR(a, 0, N - 1);
 
-void TestShallSort()
-{
-	//int a[] = { 2, 5, 8, 4, 3, 1, 9, 6 };
-	int a[] = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
-	ShallSort(a, sizeof(a) / sizeof(int));
-	for (size_t i = 0; i < sizeof(a) / sizeof(int); ++i)
-	{
-		printf("%d ", a[i]);
-	}
-}
-void TestInsretSort()
-{
-	int a[] = { 2, 5, 8, 4, 3, 1, 9, 6 };
-	InsretSort(a, sizeof(a) / sizeof(int));
-	for (size_t i = 0; i < sizeof(a) / sizeof(int); ++i)
+	for (size_t i = 0; i < N; ++i)
 	{
 		printf("%d ", a[i]);
 	}
@@ -486,12 +526,8 @@ void TestInsretSort()
 
 int main()
 {
-	//TestInsretSort();
-	//TestShallSort();
-	//TestSelectSort();
-	//TestHeapSort();
-	//TestQuickSort();
-	TestMergeSort();
+	srand((unsigned int)time(NULL));
+	TestSort();
 	system("pause");
 	return 0;
 }
